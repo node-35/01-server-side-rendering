4d0e483f5b6a4b6b19184e4d9f1d0afa36c67bfd
andres <andres.david.mm@hotmail.com>
2024-04-29 22:53:06 -0500

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,33 @@ const router = express.Router();
 
 // colocar las rutas aquí
 router.get('/', (req, res) => {
-    res.send("Welcome to express");
+    res.render("home");
 });
 
+router.get('/services', (req, res) => {
+    res.render('services');
+});
+
+router.get('/team', (req, res) => {
+    res.render('workers');
+});
+
+router.get('/team/:name', (req, res) => {
+    const { name } = req.params;
+    res.render("team-member", { name });
+});
+
+router.get('/about-me', (req, res) => {
+    res.render("about-me");
+});
+
+router.get('/projects', (req, res) => {
+    res.render('projects');
+});
+
+router.get('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    res.render('project-detail', { id });
+});
 
 module.exports = router;
